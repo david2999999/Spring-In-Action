9c2aec74c9b53117749af34966a99f8a2529afca
David Jiang <djiang86@binghamton.edu>
2018-03-22 20:22:59 +0000

    advanced bean wiring

---
+++ Advanced-Bean-Wiring/src/main/java/com/springinaction/config/DevelopmentProfileConfig.java
@@ -0,0 +1,26 @@
+package com.springinaction.config;
+
+import javax.activation.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@Profile("dev")
+public class DevelopmentProfileConfig {
+	
+	@Bean(destroyMethod="shutdown")
+	public EmbeddedDatabase dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2)
+				.addScript("classpath:schema.sql")
+				.addScript("classpath:test-data.sql")
+				.build();
+	}
+	
+
+}
